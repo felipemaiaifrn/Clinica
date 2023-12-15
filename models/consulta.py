8316ac952bc50618f9dc0925887aeee4b767055f
felipemaiaifrn <felipe.cortez@academico.ifrn.edu.br>
2023-12-14 21:52:44 -0300

    Tarefa 3 - Classes Modelo e Persistência

---
+++ models/consulta.py
@@ -0,0 +1,123 @@
+import json
+import datetime
+
+class Consulta:
+  def __init__(self, id, data, descricao, confirmado, id_paciente, id_medico):
+    self.__id = id
+    self.__data = data
+    self.__descricao = descricao
+    self.__confirmado = confirmado
+    self.__id_paciente = id_paciente
+    self.__id_medico = id_medico
+
+  def get_id(self): return self.__id
+  def get_data(self): return self.__data
+  def get_descricao(self): return self.__descricao
+  def get_confirmado(self): return self.__confirmado
+  def get_id_paciente(self): return self.__id_paciente
+  def get_id_medico(self): return self.__id_medico
+
+  def set_id(self, id): self.__id = id
+  def set_data(self, data): self.__data = data
+  def set_descricao(self, descricao): self.__data = descricao
+  def set_confirmado(self, confirmado): self.__confirmado = confirmado
+  def set_id_paciente(self, id_paciente): self.__id_paciente = id_paciente
+  def set_id_medico(self, id_medico): self.__id_medico = id_medico
+
+  def __eq__(self, x):
+    if self.__id == x.__id 
+    and self.__data == x.__data 
+    and self.__confirmado == x.__confirmado 
+    and self.__descricao == x.__descricao 
+    and self.__id_paciente == x.__id_paciente 
+    and self.__id_medico == x.__id_medico:
+      return True
+    return False
+
+  def __str__(self):
+    return f"{self.__id} - {self.__data.strftime('%d/%m/%Y %H:%M')} - {self.__descricao} - {self.__confirmado} - {self.__id_paciente} - {self.__id_medico}"
+
+  def to_json(self):
+    return {
+      'id': self.__id,
+      'data': self.__data.strftime('%d/%m/%Y %H:%M'),
+      'descricao': self.__descricao,
+      'confirmado': self.__confirmado,
+      'id_paciente': self.__id_paciente,
+      'id_medico': self.__id_medico}
+
+
+class NConsulta:
+  __consultas = []
+
+  @classmethod
+  def inserir(cls, obj):
+    cls.abrir()
+    id = 0
+    for aux in cls.__consultas:
+      if aux.get_id() > id: id = aux.get_id()
+    obj.set_id(id + 1)
+    cls.__consultas.append(obj)
+    cls.salvar()
+
+  @classmethod
+  def listar(cls):
+    cls.abrir()
+    return cls.__consultas
+
+  @classmethod
+  def listar_nao_confirmados(cls):
+    cls.abrir()
+    nao_confirmados = []
+    aux = datetime.datetime.now()
+    hoje = datetime.datetime(aux.year, aux.month, aux.day)
+    for aux in cls.__consultas:
+      if not aux.__confirmado and aux.__data > hoje:
+        nao_confirmados.append(aux)
+    return nao_confirmados
+
+  @classmethod
+  def listar_id(cls, id):
+    cls.abrir()
+    for obj in cls.__consultas:
+      if obj.get_id() == id: return obj
+    return None
+
+  @classmethod
+  def atualizar(cls, obj):
+    cls.abrir()
+    aux = cls.listar_id(obj.get_id())
+    if aux is not None:
+      aux.set_data(obj.get_data())
+      aux.set_descricao(obj.get_descricao())
+      aux.set_confirmado(obj.get_confirmado())
+      aux.set_id_paciente(obj.get_id_paciente())
+      aux.set_id_medico(obj.get_id_medico())
+      cls.salvar()
+
+  @classmethod
+  def excluir(cls, obj):
+    cls.abrir()
+    aux = cls.listar_id(obj.get_id())
+    if aux is not None:
+      cls.__consultas.remove(aux)
+      cls.salvar()
+
+  @classmethod
+  def abrir(cls):
+    cls.__consultas = []
+    try:
+      with open("consultas.json", mode="r") as arquivo:
+        consultas_json = json.load(arquivo)
+        for obj in consultas_json:
+          aux = Consulta(
+            obj["id"],
+            datetime.datetime.strptime(obj["data"], "%d/%m/%Y %H:%M"), obj["descricao"], obj["confirmado"], obj["id_paciente"], obj["id_medico"])
+          cls.__consultas.append(aux)
+    except FileNotFoundError:
+      pass
+
+  @classmethod
+  def salvar(cls):
+    with open("consultas.json", mode="w") as arquivo:
+      json.dump(cls.__consultas, arquivo, default=Consulta.to_json, indent=2)
---
+++ models/paciente.py
@@ -0,0 +1,93 @@
+import json
+
+class Paciente:
+
+    def __init__(self, id, nome, cpf, fone, senha):
+        self.__id = id
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__fone = fone
+        self.__senha = senha
+
+    def get_id(self): return self.__id
+    def get_nome(self): return self.__nome
+    def get_cpf(self): return self.__cpf
+    def get_fone(self): return self.__fone
+    def get_senha(self): return self.__senha
+
+    def set_id(self, id): self.__id = id
+    def set_nome(self, nome): self.__nome = nome
+    def set_cpf(self, cpf): self.__cpf = cpf
+    def set_fone(self, fone): self.__fone = fone
+    def set_senha(self, senha): self.__senha = senha
+
+    def __str__(self): return f"{self.__id} - {self.__nome} - {self.__cpf} - {self.__fone}"
+    def __eq__(self, x):
+        if self.__id == x.__id and self.__nome == x.__nome and self.__cpf == x.__cpf and self.__fone == x.__fone and self.__senha == x.__senha:
+            return True
+        return False
+   
+class NPaciente:
+    __pacientes = []
+   
+    @classmethod
+    def inserir(cls, x):
+        NPaciente.abrir()
+        id = 0
+        for paciente in cls.__pacientes:
+            if paciente.get_id() > id: id = paciente.get_id()
+        x.set_id(id+1)
+        for c in cls.__Pacientes:
+            if c.get_email() == x.get_email():
+                return False
+        cls.__pacientes.append(x)
+        NPaciente.salvar()
+
+    @classmethod
+    def listar(cls):
+        NPaciente.abrir()
+        return cls.__pacientes
+
+    @classmethod
+    def listar_id(cls, id):
+        NPaciente.abrir()
+        for paciente in cls.__pacientes:
+            if paciente.get_id() == id:
+                return paciente
+        return None    
+
+    @classmethod
+    def atualizar(cls, obj):
+        cls.abrir()
+        paciente = cls.listar_id(obj.get_id())
+        if paciente is not None:
+            paciente.set_nome(obj.get_nome)
+            paciente.set_cpf(obj.get_cpf)
+            paciente.set_fone(obj.get_fone)
+            paciente.set_senha(obj.get_senha)
+            NPaciente.salvar()
+
+    @classmethod
+    def excluir(cls, obj):
+        cls.abrir()
+        p = cls.listar_id(obj.get_id())
+        if p is not None:
+            cls.__pacientes.remove(p)
+            NPaciente.salvar()
+
+    @classmethod
+    def abrir(cls):
+        cls.__pacientes = []
+        try:
+            with open("pacientes.json", mode="r") as arquivo:
+                pacientes_json = json.load(arquivo)
+                for obj in pacientes_json:
+                    p = Paciente(obj["_Paciente__id"], obj["_Paciente__nome"], obj["_Paciente__email"], obj["_Paciente__fone"], obj["_Paciente__senha"])
+                    cls.__pacientes.append(p)
+        except FileNotFoundError:
+            pass
+
+    @classmethod
+    def salvar(cls):
+        with open('pacientes.json', mode='w') as arquivo:
+            json.dump(cls.__pacientes, arquivo, default=vars, indent=2)
